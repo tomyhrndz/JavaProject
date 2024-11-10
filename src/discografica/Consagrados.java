3075fe727925fe5ec8dae2f00fb66a93c97726cd
Tomás <tomas.hrndz1@gmail.com>
2024-11-10 19:57:43 -0300

    Liquidacion

--- src/discografica/Consagrados.java
+++ src/discografica/Consagrados.java
@@ -11,32 +11,5 @@ public class Consagrados extends Artista implements Serializable {
 
     @Override
     public boolean EsEmergente(){ return false; }
-
-    /*
-    @Override
-    public Liquidacion getLiquidacion()
-    {
-        Liquidacion LiquidacionGanancia = new Liquidacion();
-
-        for(Disco disco : getDiscos())
-        {
-            LiquidacionGanancia.LiquidacionReproducciones.addAll(disco.GetGananciaReproducciones(false));
-            LiquidacionGanancia.LiquidacionDisco.add(disco.GetGananciaDisco(false));
-        }
-
-        int ActMonth = LocalDate.now().getMonthValue();
-        int ActYear = LocalDate.now().getYear();
-        LocalDate DateRecital;
-
-        for(Recital recital : getRecitales())
-        {
-            DateRecital =  recital.GetFecha();
-            if(DateRecital.getMonthValue() == ActMonth && DateRecital.getYear() == ActYear)
-                LiquidacionGanancia.LiquidacionRecitales.add(recital.GetGananciaRecital(false));
-        }
-
-        return LiquidacionGanancia;
-    }
-     */
 }
 
--- src/discografica/Emergentes.java
+++ src/discografica/Emergentes.java
@@ -8,32 +8,5 @@ public class Emergentes extends Artista implements Serializable {
     public Emergentes(String ID, String Nombre, int CantIntegrantes, String Genero) {
         super(ID, Nombre, CantIntegrantes, Genero);
     }
-
-    /*
-    @Override
-    public Liquidacion getLiquidacion()
-    {
-        Liquidacion LiquidacionGanancia = new Liquidacion();
-
-        for(Disco disco : getDiscos())
-        {
-            LiquidacionGanancia.LiquidacionReproducciones.addAll(disco.GetGananciaReproducciones(true));
-            LiquidacionGanancia.LiquidacionDisco.add(disco.GetGananciaDisco(true));
-        }
-
-        int ActMonth = LocalDate.now().getMonthValue();
-        int ActYear = LocalDate.now().getYear();
-        LocalDate DateRecital;
-
-        for(Recital recital : getRecitales())
-        {
-            DateRecital =  recital.GetFecha();
-            if(DateRecital.getMonthValue() == ActMonth && DateRecital.getYear() == ActYear)
-                LiquidacionGanancia.LiquidacionRecitales.add(recital.GetGananciaRecital(true));
-        }
-
-        return LiquidacionGanancia;
-    }
-     */
 }
 
