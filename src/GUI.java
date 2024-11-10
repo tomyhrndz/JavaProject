88acf8e5b08233c0482d7484bdc524a3ee88e9c8
Dante <dantesuarez2003@gmail.com>
2024-11-10 14:04:18 -0300

    Correccion Lectura XML
    Comprueba que el archivo este completo antes de cargar y se reemplazo un if por try

--- src/GUI.java
+++ src/GUI.java
@@ -8,6 +8,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 
@@ -205,12 +207,11 @@ public class GUI {
                             @Override
                             public void actionPerformed(ActionEvent e) {
                                 String Path = archivoTextField.getText() + ".xml";
-                                File Arch = new File(Path);
-                                if (Arch.exists()) {
+                                try {
                                     Spotify.CargaDatos(Path);
                                     JOptionPane.showMessageDialog(frame, "El archivo se cargó de forma correcta.", "Éxito", JOptionPane.INFORMATION_MESSAGE);
                                     cardLayout.show(mainPanel, "artistaPanel");
-                                } else {
+                                } catch (FileNotFoundException ex) {
                                     JOptionPane.showMessageDialog(frame, "El archivo no existe.", "Error", JOptionPane.INFORMATION_MESSAGE);
                                 }
                             }
---
+++ src/exceptions/ErroresEnArchivoException.java
@@ -0,0 +1,6 @@
+package exceptions;
+
+public class ErroresEnArchivoException extends Throwable {
+    public ErroresEnArchivoException(String string) {
+    }
+}
